002ae9a5d7b1ab483fba9dbabf6db214b14253e1
ModStart <modstart@163.com>
2024-12-23 10:30:24 +0800

    新增：Mac（Apple芯片）支持声音克隆和声音合成

--- src/pages/Video.vue
+++ src/pages/Video.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 
 import {t} from "../lang";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import SoundTts from "./Sound/SoundTts.vue";
 import SoundClone from "./Sound/SoundClone.vue";
 import VideoGen from "./Video/VideoGen.vue";
 
-const tab = ref('videoGen');
+const platformName = ref<'win' | 'osx' | 'linux' | null>(null);
+const tab = ref('soundClone');
+
+onMounted(() => {
+    platformName.value = window.$mapi.app.platformName();
+});
 
 </script>
 
@@ -14,11 +19,11 @@ const tab = ref('videoGen');
     <div class="pb-device-container h-full relative select-none flex">
         <div class="p-5 w-52 flex-shrink-0 border-r border-solid border-gray-100">
             <div class="p-2 rounded-lg mr-2 mb-4 cursor-pointer"
-                 :class="tab === 'videoGen' ? 'bg-gray-100' : ''"
-                 @click="tab = 'videoGen'">
+                 :class="tab === 'soundClone' ? 'bg-gray-100' : ''"
+                 @click="tab = 'soundClone'">
                 <div class="text-base">
-                    <i class="iconfont icon-video"></i>
-                    {{ t('视频合成') }}
+                    <i class="iconfont icon-sound-clone"></i>
+                    {{ t('声音克隆') }}
                 </div>
             </div>
             <div class="p-2 rounded-lg mr-2 mb-4 cursor-pointer"
@@ -30,17 +35,18 @@ const tab = ref('videoGen');
                 </div>
             </div>
             <div class="p-2 rounded-lg mr-2 mb-4 cursor-pointer"
-                 :class="tab === 'soundClone' ? 'bg-gray-100' : ''"
-                 @click="tab = 'soundClone'">
+                 v-if="platformName && ['win'].includes(platformName)"
+                 :class="tab === 'videoGen' ? 'bg-gray-100' : ''"
+                 @click="tab = 'videoGen'">
                 <div class="text-base">
-                    <i class="iconfont icon-sound-clone"></i>
-                    {{ t('声音克隆') }}
+                    <i class="iconfont icon-video"></i>
+                    {{ t('视频合成') }}
                 </div>
             </div>
         </div>
         <div class="flex-grow h-full overflow-y-auto">
-            <SoundTts v-if="tab === 'soundTts'"/>
-            <SoundClone v-else-if="tab === 'soundClone'"/>
+            <SoundClone v-if="tab === 'soundClone'"/>
+            <SoundTts v-else-if="tab === 'soundTts'"/>
             <VideoGen v-else-if="tab === 'videoGen'"/>
         </div>
     </div>
